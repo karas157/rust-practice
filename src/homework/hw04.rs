4f358f188efec54aed674b5de523d479579b8801
karas157 <malontaras@gmail.com>
2025-03-11 21:11:23 +0200

    Update hw04.rs

---
+++ src/homework/hw04.rs
@@ -0,0 +1,28 @@
+fn print_diamond(size: usize) {
+    let mid = size / 2;
+
+    for i in 0..=mid {
+        for _ in 0..(mid - i) {
+            print!("  ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("* ");
+        }
+        println!();
+    }
+
+    for i in (0..mid).rev() {
+        for _ in 0..(mid - i) {
+            print!("  ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("* ");
+        }
+        println!();
+    }
+}
+
+fn main() {
+    let size = 21; // Размер ромба должен быть нечетным
+    print_diamond(size);
+}
