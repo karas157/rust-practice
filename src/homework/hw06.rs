9575659e4c186cea274e04908305fe43bab70544
karas157 <malontaras@gmail.com>
2025-03-15 17:36:37 +0200

    Update hw06.rs

--- src/homework/hw06.rs
+++ src/homework/hw06.rs
@@ -2,7 +2,7 @@ fn draw_tree() {
     let star_counts = vec![1, 3, 5, 7, 9];
 
     for stars_in_row in &star_counts {
-        let width = star_counts[4]; // максимальна ширина
+        let width = star_counts[4]; 
         for row in 0..*stars_in_row / 2 + 1 {
             let stars = 2 * row + 1;
             let spaces = (width - stars) / 2;
